e68ae27cf767a9bf3fea4a10d92f463e210b45c6
Michael Deaver <equationslay@ufl.edu>
2022-04-13 10:24:36 -0400

    macOS path test 2

--- DjangoWebProject/DjangoWebProject/settings.py
+++ DjangoWebProject/DjangoWebProject/settings.py
@@ -86,10 +86,10 @@ if sys.platform.startswith('win32'):
     # print(isDir)
     cx_Oracle.init_oracle_client(lib_dir="/oracle/instantclient_21_3")
 elif sys.platform.startswith('darwin'):
-    isDir = os.path.isdir("/Users/local/Downloads/instantclient_19_8")
+    isDir = os.path.isdir(os.path.expanduser("~/Downloads/instantclient_19_8"))
     print(isDir)
     print("macOS")
-    cx_Oracle.init_oracle_client(lib_dir="~/Downloads/instantclient_19_8")
+    cx_Oracle.init_oracle_client(lib_dir=os.path.expanduser("~/Downloads/instantclient_19_8"))
 elif sys.platform.startswith('linux'):
     # isDir = os.path.isdir("/oracle/instantclient_21_5")
     # print(isDir)

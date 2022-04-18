3541387efda17cafca8cd5681a8da04b86f114c1
abdoulkkonate <73804112+abdoulkkonate@users.noreply.github.com>
2022-04-18 19:45:35 -0400

    Log in interface
    Log in interface

--- DjangoWebProject/TeacherApp/views.py
+++ DjangoWebProject/TeacherApp/views.py
@@ -3,12 +3,18 @@ Definition of views.
 """
 
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponseRedirect
 from .models import Teacher, Student, Dra, Ireadymath, Ireadyreading
 from django.core import serializers
+from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
+#Create user view here
+from .models import *
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     """Renders the home page."""
@@ -53,16 +59,51 @@ def about(request):
 
 def register(request):
     """Renders the teacher registration page."""
-    form = UserCreationForm()
-    return render(
-        request,        
-        'app/register.html',
-         {
-             'form': form,
-             'title': 'Register Today',
-             'year': datetime.now().year,
-         }
-    )
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+
+            return redirect('login')
+
+    context = {'form' :form}
+    return render(request, 'app/register.html', context)
+    
+    # return render(
+    #     request,        
+    #     'app/register.html',
+    #      {
+    #          'form': form,
+    #          'title': 'Register Today',
+    #          'year': datetime.now().year,
+    #      }
+    # )
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'app/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 
 def searchtest(request):
